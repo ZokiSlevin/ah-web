9aba3ac0909494dd733497b718fe89f996293def
zoran.macesic@solera.com <zoran.macesic@solera.com>
2025-11-27 15:19:23 +0100

    Initial Streamlit AH test

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="MEVA - AH Statistika", layout="wide")
+
+st.title("MEVA - AH Statistika (web test)")
+st.write("Ako ovo vidiš u browseru na streamlit domeni – deploy radi ✅")
